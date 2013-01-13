bde38f4fc9fd123f9c3aad810f68491103620012
John Asmuth <jasmuth@gmail.com>
2013-01-13 15:07:27 -0500

    init

---
+++ debugtags.go
@@ -0,0 +1,56 @@
+package debugtags
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+var spaces = "                                                                  "
+
+type Tracer struct {
+	Enabled bool
+	level   int
+}
+
+func (t *Tracer) In(name string, items ...interface{}) {
+	if t.Enabled {
+		fmt.Printf("%s+%s %v\n", spaces[:t.level], name, items)
+	}
+	t.level++
+}
+
+func (t *Tracer) Out(name string, items ...interface{}) {
+	if e := recover(); e != nil {
+		panic(e)
+	}
+	t.level--
+	if t.Enabled {
+		fmt.Printf("%s-%s %v\n", spaces[:t.level], name, items)
+	}
+}
+
+func (t *Tracer) Println(items ...interface{}) {
+	if t.Enabled {
+		fmt.Println(items...)
+	}
+}
+func (t *Tracer) Printf(format string, items ...interface{}) {
+	if t.Enabled {
+		str := fmt.Sprintf(format, items...)
+		str = strings.TrimSpace(str)
+		fmt.Printf("%s %s\n", spaces[:t.level], str)
+	}
+}
+
+func (t *Tracer) JSON(item interface{}) {
+	if t.Enabled {
+		var b1 bytes.Buffer
+		enc := json.NewEncoder(&b1)
+		enc.Encode(item)
+		var b2 bytes.Buffer
+		json.Indent(&b2, b1.Bytes(), "", " ")
+		fmt.Println(b2.String())
+	}
+}
